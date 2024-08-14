7c954341421f9b367c034b33d6032c6de927fe43
Deepak Poojary <63220831+deepakpoojary@users.noreply.github.com>
2024-08-14 22:47:14 +0530

    Time: 8 ms (33.94%), Space: 8.7 MB (72.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
-    bool is_valid(char ch){
-    if((ch>='a'&& ch<='z')||(ch>='0'&& ch<='9')||(ch>='A'&& ch<='Z'))
-    {return 1;}
-        return 0;
-    }
-    
-    bool to_lower(char ch){
-    if(ch>='A'&& ch<='Z')
-    {return 1;
-    }
-    return 0;
-    }  
-    
     bool isPalindrome(string s) {
-        //elligible candidate
-        //converting to lower case
-        string a="";
-        
-        for(int i=0;i<s.length();i++){
-            if(is_valid(s[i])){
-                if(to_lower(s[i])){
-                    s[i]=s[i]+32;
-                }
-            a.push_back(s[i]);
-               }}
-    
-        
-        
-        
-        // check_palindrome
-        int i=0;
-        int j=a.length()-1;
-        while(i<j){
-            if(a[i]!=a[j]){
-               return 0;
-            }
-            i++;
-            j--;
-            }
-        return 1;
-        
+        int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
     }
 };
