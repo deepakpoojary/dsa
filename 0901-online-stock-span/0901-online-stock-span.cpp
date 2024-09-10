cacc075d08846c964fe0be77c61079f73683c7ca
Deepak Poojary <63220831+deepakpoojary@users.noreply.github.com>
2024-09-10 18:02:11 +0530

    Time: 163 ms (81.73%), Space: 90.4 MB (37.70%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,17 +1,18 @@
 class StockSpanner {
 public:
+    stack<pair<int, int>> s;
     StockSpanner() {//this will be called each time 
         
-    }
-        stack<pair<int, int>> s;
 
+    }
+        
     int next(int price) {
           int res = 1;
         while (!s.empty() && s.top().first <= price) {
             res += s.top().second;
             s.pop();
         }
-        s.push({price, res});
+        s.push({price, res});//store the cumilative ans 
         return res;
     }
 };
