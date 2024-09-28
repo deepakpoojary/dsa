6757264d83f4934751b22552def892cf61bf589d
Deepak Poojary <63220831+deepakpoojary@users.noreply.github.com>
2024-09-28 11:49:28 +0530

    Time: 52 ms (96.60%), Space: 85.9 MB (83.45%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        // Count of $5 and $10 bills in hand
-        int fiveDollarBills = 0;
-        int tenDollarBills = 0;
-
-        // Iterate through each customer's bill
-        for (int customerBill : bills) {
-            if (customerBill == 5) {
-                // Just add it to our count
-                fiveDollarBills++;
-            } else if (customerBill == 10) {
-                // We need to give $5 change
-                if (fiveDollarBills > 0) {
-                    fiveDollarBills--;
-                    tenDollarBills++;
-                } else {
-                    // Can't provide change, return false
-                    return false;
+        int five = 0, ten = 0;//change only these 2
+        
+        for (int bill:bills ){
+            if(bill==5){
+                five++;
+            }
+            else if(bill==10){
+                if(five==0){
+                    return 0;
                 }
-            } else {  // customerBill == 20
-                // We need to give $15 change
-                if (tenDollarBills > 0 && fiveDollarBills > 0) {
-                    // Give change as one $10 and one $5
-                    fiveDollarBills--;
-                    tenDollarBills--;
-                } else if (fiveDollarBills >= 3) {
-                    // Give change as three $5
-                    fiveDollarBills -= 3;
-                } else {
-                    // Can't provide change, return false
-                    return false;
+                five--;
+                ten++;
+                
+            }
+            else {
+                 if(five >0 && ten >0){
+                    five-=1;
+                    ten-=1;
                 }
+                else if(five>=3){
+                    five-=3;
+                }
+                
+                else {return 0;}
+                
             }
         }
-        // If we've made it through all customers, return true
-        return true;
-    }
-};
+        return 1;
+        
+
+    
+}};
