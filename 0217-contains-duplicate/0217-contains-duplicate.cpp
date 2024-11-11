1acf57a23cda69db44ba166b21bfac77f411d9f7
Deepak Poojary <63220831+deepakpoojary@users.noreply.github.com>
2024-11-11 22:03:56 +0530

    Time: 27 ms (76.18%), Space: 73.5 MB (48.63%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,8 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map <int,int> mp;
         for(auto it:nums){
-            mp[it]++;
-            if(mp[it]>1)return 1;
+            
+            if(++mp[it]>1)return 1;
         }
         return 0;
     }
